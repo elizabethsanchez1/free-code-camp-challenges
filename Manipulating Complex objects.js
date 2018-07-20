e5f628ed15c0a22c62a56805658e394237186be4
jose sanchez <sanchez24jose@gmail.com>
2018-07-19 21:30:06 -0500

    answer to Manipulating Complex Objects

---
+++ Manipulating Complex objects.js
@@ -0,0 +1,27 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, 
+  {
+    "artist": "Beyonce",
+    "title": "All the single ladies",
+    "release_year": 2000,
+    "formats": [
+      "Cds",
+      "New",
+      "Single"
+
+        
+    ]
+  }
+
+  
+];
